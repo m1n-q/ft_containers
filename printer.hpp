c8dab76e01f755eb304d9927e5d2cae578e1a756
m1n-q <m1n_q@naver.com>
2022-03-27 22:46:09 +0900

    find time consumer : get_bf()

--- printer.hpp
+++ printer.hpp
@@ -6,13 +6,16 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 20:50:52 by mishin            #+#    #+#             */
-/*   Updated: 2022/03/21 21:15:31 by mishin           ###   ########.fr       */
+/*   Updated: 2022/03/27 22:44:42 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTER_HPP
 # define PRINTER_HPP
 # include <iostream>
+#include <chrono>
+# define START	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+# define END	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();std::cout<< "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
 
 void	print_header(int input)
 {
