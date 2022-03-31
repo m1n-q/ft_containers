74e15ae7a9681586e7e292ffd5300c70793273f9
m1n-q <m1n_q@naver.com>
2022-03-31 15:51:28 +0900

    make vector testcase

--- main_test.cpp
+++ main_test.cpp
@@ -20,12 +20,19 @@ template<
 		>
 void		map_tester()
 {
-	Map<>	m;
+	// Map<>	m;
 
-	m.insert(Pair<>(1, 'a'));
+	// m.insert(Pair<>(1, 'a'));
+	// m.insert(Pair<>(2, 'a'));
+	// m.insert(Pair<>(3, 'a'));
+	// m.insert(Pair<>(4, 'a'));
+	// m.insert(Pair<>(4, 'a'));
+	// m.insert(Pair<>(6, 'a'));
+	// m.insert(Pair<>(7, 'a'));
+	// m.insert(Pair<>(8, 'a'));
 
-	std::cout << m.begin()->first << std::endl;
-	std::cout << m.begin()->second << std::endl;
+	// std::cout << m.begin()->first << std::endl;
+	// std::cout << m.begin()->second << std::endl;
 
 }
 
@@ -35,15 +42,153 @@ template<
 		>
 void		vector_tester()
 {
-	Vec<> v;
-	v.push_back(42);
 
-	std::cout << v[0] << std::endl;
+	T		arr[4] = {11, 22, 33, 44};
+
+	Vec<>	v1;
+	Vec<>	v2(3, 42);
+	Vec<>	v3(arr, arr + 4);
+
+	PRINT(CYAN("v1 ( default constructed ) =>"))
+	print_vector(v1);
+	PRINT(YELLOW("v2 ( fill constructed ) =>"))
+	print_vector(v2);
+	PRINT(BLUE("v3 ( range constructed ) =>"))
+	print_vector(v3);
+
+
+
+	const Vec<> cv(v3);
+
+	v3[0] = 1111;
+
+	PRINT(GREEN("const cv ( copy constructed, from v3 ) "))
+	print_vector(cv);
+	PRINT(YELLOW("v3[0]")" changed")
+	print_vector(v3);
+
+
+	// begin()			// @ C
+	// end()			// @ C
+	// rbegin()			// @ C
+	// rend()			// @ C
+
+//' capacity
+	PRINT("max size of vector =>" )
+	std::cout << v1.max_size() << std::endl;
+	PRINT("max size of const vector =>" )
+	std::cout << cv.max_size() << std::endl;	// @ C
+
+	std::cout << CYAN("v1") " is Empty?\t" << v1.empty() << std::endl;
+	std::cout << GREEN("cv") " is Empty?\t" << cv.empty() << std::endl;	// @ C
+
+	// size()		// @ C
+	// capacity()	// @ C
+
+	PRINT("current state of" YELLOW("v2"))
+	print_vector(v2);
+
+	PRINT("after "YELLOW("v2") ".reserve(1)")
+	v2.reserve(1);
+	print_vector(v2);
+
+	PRINT("after "YELLOW("v2") ".reserve(10)")
+	v2.reserve(10);
+	print_vector(v2);
+
+
+	//TODO:resize()
+
+//' element
+
+	PRINT(YELLOW("v2[0]") " => ")
+	std::cout << v2[0] << std::endl;
+	v2[0] = 424242;
+
+	PRINT(YELLOW("v2[0]") " changed => ")
+	print_vector(v2);
+
+	PRINT(GREEN("const cv[0]") " => ")
+	std::cout << cv[0] << std::endl;		// @ C
+
+// {	cv[0] = 1;	// cannot assign to const iter
+
+
+	PRINT("const cv.at(10) => ")
+	try							{ std::cout<< cv.at(10) << std::endl; } 	// @ C
+	catch (std::exception& e)	{ std::cerr << e.what() << std::endl; }
+
+
+	PRINT("front() && back() of const cv")
+	std::cout << cv.front() << std::endl;	// @ C
+	std::cout << cv.back() << std::endl;	// @ C
+
+
+	PRINT("current state of v1")
+	print_vector(v1);
+	for (int i = 0; i < 42; i++)
+		v1.push_back(i);
+	PRINT("after push_back " RED("[") "0, 42 " RED(")"))
+	print_vector(v1);
+
+
+	PRINT(CYAN("v1: ") "assign (fill n)")
+	v1.assign(3, 99);
+	print_vector(v1);
+
+	PRINT(CYAN("v1.pop_back()"))
+	v1.pop_back();	// try at empty vector
+	print_vector(v1);
+
+
+	PRINT(CYAN("v1: ") "assign " RED("[ ") GREEN("cv") ".begin()," GREEN("cv") ".end()" RED(" )"))
+	v1.assign(cv.begin(), cv.end());
+	print_vector(v1);
+
+
+	PRINT("insert " PURPLE ("123456 ") "at " CYAN("v1.begin"))
+	v1.insert(v1.begin(), 123456);
+	print_vector(v1);
+
+
+	PRINT("insert " PURPLE ("[777] * 3 ")  "at " CYAN("v1.end"))
+	v1.insert(v1.end(), 3, 777);
+	print_vector(v1);
+
+
+	PRINT("insert " YELLOW("v2 ") "at " CYAN("v1[3]"))
+	v1.insert(v1.begin() + 3, v2.begin(), v2.end());
+	print_vector(v1);
+
+
+	PRINT("erase " PURPLE("4th elem ")  "of " CYAN("v1"))
+	v1.erase(v1.begin() + 3);
+	print_vector(v1);
+
+	PRINT("erase " RED("[ ") "2nd, 6th " RED (") ") "of " CYAN( "v1 "))
+	v1.erase(v1.begin() + 1, v1.begin() + 5);
+	print_vector(v1);
+
+	PRINT(PURPLE("before ") "swap " "(" YELLOW ("v2") ", " BLUE ("v3") ")")
+	PRINT("v2 =>")
+	print_vector(v2);
+	PRINT("v3 =>")
+	print_vector(v3);
+
+	swap(v2, v3);
+	PRINT(PURPLE("after ") "swap "  "(" YELLOW ("v2") ", " BLUE ("v3") ")")
+
+	PRINT(YELLOW("v2 =>"))
+	print_vector(v2);
+	PRINT(BLUE("v3 =>"))
+	print_vector(v3);
+	// clear()
+	// get_allocator()	// @ C
+
 }
 
 int main(int argc, char** argv)
 {
-
 	std::cout<< std::boolalpha;
 	if (argc != 2)
 		return -1;
@@ -52,13 +197,13 @@ int main(int argc, char** argv)
 	if (!input)
 	{
 		print_header(input);
-		// vector_tester<int, std::vector>();
+		vector_tester<int, std::vector>();
 		// map_tester<int, char, std::less, std::pair, std::map >();
 	}
 	else
 	{
 		print_header(input);
-		// vector_tester<int, ft::vector>();
+		vector_tester<int, ft::vector>();
 		// map_tester<int, char, std::less, ft::pair, ft::map >();
 	}
 	system("leaks a.out");
--- printer.hpp
+++ printer.hpp
@@ -6,34 +6,61 @@
 /*   By: mishin <mishin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 20:50:52 by mishin            #+#    #+#             */
-/*   Updated: 2022/03/27 22:44:42 by mishin           ###   ########.fr       */
+/*   Updated: 2022/03/31 15:45:32 by mishin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTER_HPP
 # define PRINTER_HPP
+
 # include <iostream>
-#include <chrono>
-# define START	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-# define END	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();std::cout<< "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+# include <chrono>
+
+# define RED(s) 				"\033[1;31m"s"\033[0m"
+# define BLUE(s) 				"\033[1;34m"s"\033[0m"
+# define GRAY(s) 				"\033[0;37m"s"\033[0m"
+# define CYAN(s) 				"\033[1;36m"s"\033[0m"
+# define GREEN(s)				"\033[0;32m"s"\033[0m"
+# define YELLOW(s)				"\033[0;33m"s"\033[0m"
+# define PURPLE(s)				"\033[0;35m"s"\033[0m"
+// # define NC(s)					"\033[0m"
+# define UL(s)					"\033[4m"
+# define START					std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+# define END					std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();std::cout<< "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+# define PRINT(x)				std::cout<< x << std::endl;
 
 void	print_header(int input)
 {
 	input ? system(" artii \"FT\" | lolcat -F 0.25") : system(" artii \"STD\" | lolcat -F 0.25");
 }
 
-template <class Container>
-void	print_vector(const Container& myvector )
+template <class Vec>
+void	print_vector(const Vec& v )
 {
-	typename Container::const_iterator	it;
+	typename Vec::const_iterator	it;
 
 	std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
-	for (it = myvector.begin(); it != myvector.end(); it++)
+	for (it = v.begin(); it != v.end(); it++)
 		std::cout << "[" << *it << "] ";
 	std::cout << std::endl;
 
-	std::cout << "SIZE	=> " << myvector.size() << std::endl;
-	std::cout << "CAP	=> " << myvector.capacity() << std::endl;
+	std::cout << "CAP	=> " << v.capacity() << std::endl;
+	std::cout << "SIZE	=> " << v.size() << std::endl;
+
+	std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl << std::endl;
+}
+
+template <class Map>
+void	print_map(const Map& v )
+{
+	typename Map::const_iterator	it;
+
+	std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
+	for (it = v.begin(); it != v.end(); it++)
+		std::cout << "[" << it->first << ", " << it->second << "] ";
+	std::cout << std::endl;
+
+	std::cout << "SIZE	=> " << v.size() << std::endl;
 
 	std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl << std::endl;
 }
